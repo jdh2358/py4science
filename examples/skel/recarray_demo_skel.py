900ec978126dcdc1962a4dcb557f911812a66465
John Hunter <jdh2358@gmail.com>
2007-04-12 20:23:15 +0000

    added rec skel
    svn path=/trunk/py4science/; revision=3222

--- examples/skel/recarray_demo_skel.py
+++ examples/skel/recarray_demo_skel.py
@@ -5,6 +5,7 @@ import time, datetime, csv
 import dateutil.parser
 import numpy
 
+XXX = None
 # create a csv reader to parse the file
 fh = file('data/ge.csv')
 reader = csv.reader(fh)
@@ -14,10 +15,7 @@ header = reader.next()
 # objects, ints, or floats as approriate
 rows = []
 for date, open, high, low, close, volume, adjclose in reader:
-    date = dateutil.parser.parse(date).date()
-    volume = int(volume)
-    open, high, low, close, adjclose = map(float, (
-        open, high, low, close, adjclose))
+    XXX # conver the strings here
     rows.append((date, open, high, low, close, volume, adjclose))
 
 fh.close()
@@ -29,35 +27,9 @@ r = numpy.rec.fromrecords(rows, names='date,open,high,low,close,volume,adjclose'
 
 # compute the average approximate dollars traded over the last 10 days
 # hint: closing price * volume trades approx equals dollars trades
-recent = r[-10:]
-dollars = mean(recent.close * recent.volume)
-print '$%1.2fM'%(dollars/1e6)
+XXX
 
 # plot the adjusted closing price vs time since 2003 - hint, you must
 # use date2num to convert the date to a float for mpl.  Make two axes,
 # one for price and one for volume.  Use a bar chart for volume
-from matplotlib.dates import date2num
-import pylab
-mask = r.date>datetime.date(2003,1,1)
-date = date2num(r.date[mask])
-price = r.adjclose[mask]
-volume = r.volume[mask]
-
-fig = pylab.figure()
-ax1 = fig.add_subplot(211)
-ax1.plot(date, price, '-');
-ax1.xaxis_date()
-ax1.grid()
-for label in ax1.get_xticklabels():
-    label.set_visible(False)
-
-ax2 = fig.add_subplot(212, sharex=ax1)
-ax2.bar(date, volume);
-ax2.xaxis_date()
-ax2.grid()
-for label in ax2.get_xticklabels():
-    label.set_rotation(30)
-    label.set_horizontalalignment('right')
-
-
-pylab.show()
+XXX

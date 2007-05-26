64a91db25381ae4564121c4d61ae704518bfcada
John Hunter <jdh2358@gmail.com>
2007-05-26 13:26:51 +0000

    svn path=/trunk/py4science/; revision=3307

--- examples/spline_demo.py
+++ examples/spline_demo.py
@@ -10,19 +10,13 @@ dt2 = 0.001
 
 tfine = N.arange(0.0, 5, 0.01)
 tcoarse = N.arange(0.0, 5, 0.1)
-def func(t):
-    return N.cos(N.pi*t) * N.sin(2*N.pi*t)
+s = N.cos(N.pi*tcoarse) * N.sin(2*N.pi*tcoarse)
 
 # create sinterp by computing the spline fitting tcoarse to s and then
-# evaluating it on tfine.  Plot tcoarse vs s with markers and tfine vs
-# sinterp with a solid line
-s = func(tcoarse)
-
+evaluating it on tfine.  Plot tcoarse vs s with markers and tfine vs
+sinterp with a solid line
 tck = interpolate.splrep(tcoarse, s, s=0)
 sinterp = interpolate.splev(tfine, tck, der=0)
 
-P.plot(tcoarse, s, 'o', label='coarse')
-P.plot(tfine, sinterp, '+', label='fit')
-P.plot(tfine, func(tfine), '-', label='actual')
-P.legend()
+P.plot(tcoarse, s, 'o', tfine, sinterp)
 P.show()

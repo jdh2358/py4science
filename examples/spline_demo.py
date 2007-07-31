b5686f9486c3f909972f22b435043f4704b8c820
Fernando Perez <fernando.perez@berkeley.edu>
2007-07-31 06:53:07 +0000

    Put in changes that for some reason SVN was not seeing in my local copy.  Problem reported by Stefan
    svn path=/trunk/py4science/; revision=3643

--- examples/spline_demo.py
+++ examples/spline_demo.py
@@ -10,13 +10,19 @@ dt2 = 0.001
 
 tfine = N.arange(0.0, 5, 0.01)
 tcoarse = N.arange(0.0, 5, 0.1)
-s = N.cos(N.pi*tcoarse) * N.sin(2*N.pi*tcoarse)
+def func(t):
+    return N.cos(N.pi*t) * N.sin(2*N.pi*t)
 
 # create sinterp by computing the spline fitting tcoarse to s and then
-evaluating it on tfine.  Plot tcoarse vs s with markers and tfine vs
-sinterp with a solid line
+# evaluating it on tfine.  Plot tcoarse vs s with markers and tfine vs
+# sinterp with a solid line
+s = func(tcoarse)
+
 tck = interpolate.splrep(tcoarse, s, s=0)
 sinterp = interpolate.splev(tfine, tck, der=0)
 
-P.plot(tcoarse, s, 'o', tfine, sinterp)
+P.plot(tcoarse, s, 'o', label='coarse')
+P.plot(tfine, sinterp, '+', label='fit')
+P.plot(tfine, func(tfine), '-', label='actual')
+P.legend()
 P.show()

45a8bbb727fc18c4ed370dc928c4050c7f5752ed
Jeff Whitaker <jswhit@fastmail.fm>
2007-12-03 15:49:35 +0000

    add an OPenDAP example URL
    svn path=/trunk/py4science/; revision=4563

--- examples/skel/basemap5_skel.py
+++ examples/skel/basemap5_skel.py
@@ -5,6 +5,9 @@ import pylab, numpy
 # or (if PyNIO is installed) a GRIB or HDF file.
 # See http://nomads.ncdc.noaa.gov/ for some NOAA OPenDAP datasets.
 ncfile = NetCDFFile('data/sst.nc')
+# this will produce a very similar plot, but will read
+# the data over the web instead of from a local file.
+#ncfile = NetCDFFile('http://nomads.ncdc.noaa.gov:8085/thredds/dodsC/oisst/2007/AVHRR/sst4-navy-eot.20071201.nc')
 sst = ncfile.variables['analysed_sst'][:]
 lats = ncfile.variables['lat'][:]
 lons = ncfile.variables['lon'][:]
